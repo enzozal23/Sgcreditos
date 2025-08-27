fb2f68684ca937878a2e21d5570c9bc42f41604e
Enzo <enzo.z.1.ez@gmail.com>
2025-08-26 21:55:51 -0300

    probando webhook

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Nueva ruta personalizada prueba de webhook 21:48
+// Nueva ruta personalizada prueba de webhook 21:55 alalalal
 Route::get('/bienvenido', function () {
     return view('bienvenido');
 });
